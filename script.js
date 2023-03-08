a29e59c2d1060beaf050a3e9af6d2c35726057dc
GioCura <125846707+GioCura@users.noreply.github.com>
2023-03-08 10:15:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const cardItemEl = document.querySelectorAll(".card__item");
-const cardAnswerActive = document.querySelector(".card__answer--active");
 
-cardItemEl.forEach((cardItemEl) => {
-  cardItemEl.addEventListener("click", () => {
-    cardItemEl.classList.toggle("card__answer--active");
+// iterates through each element with ".card__item"
+cardItemEl.forEach((el) => {
+  // for each element, there's an event that triggers on click
+  el.addEventListener("click", () => {
+    // for each clicked ".card__item", the following function should be done
+    cardItemEl.forEach(function (item) {
+      // this checks elements with the active class and removes them.
+      if (item.classList.contains("card__answer--active")) {
+        item.classList.remove("card__answer--active");
+        // if the item is the one that was clicked, this adds the active state
+      } else if (item === el) {
+        item.classList.add("card__answer--active");
+        item.style.height = item.scrollHeight;
+      }
+    });
   });
 });
