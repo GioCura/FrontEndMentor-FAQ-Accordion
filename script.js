155dafe3a1c800af6ecda15d0aa1cfc0bea0f523
GioCura <125846707+GioCura@users.noreply.github.com>
2023-03-09 08:42:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const cardItemEl = document.querySelectorAll(".card__item");
+let activeAnswerEl = document.querySelector(
+  ".card__answer--active .card__answer"
+);
 
-// iterates through each element with ".card__item"
+// Reveals the answer that is meant to be shown on load.
+function preloadAnswer() {
+  activeAnswerEl.style.height = activeAnswerEl.scrollHeight + "px";
+}
+
+// Hides the answer
+function closeAnswer(e) {
+  e.classList.remove("card__answer--active");
+  e.children[1].style.height = "0";
+  e.children[1].style.transition = "height 0.5s";
+}
+
+// Reveals the answer
+function openAnswer(e) {
+  e.classList.add("card__answer--active");
+  e.children[1].style.height = e.children[1].scrollHeight + "px";
+  e.children[1].style.transition = "height 0.5s";
+  e.children[1].style.animation = "fade 0.5s ease-in-out";
+}
+
+// Iterates through each element with ".card__item"
 cardItemEl.forEach((el) => {
-  // for each element, there's an event that triggers on click
+  // For each element, there's an event that triggers on click
   el.addEventListener("click", () => {
-    // for each clicked ".card__item", the following function should be done
+    // For each clicked ".card__item", the following should be done
     cardItemEl.forEach(function (item) {
-      // this checks elements with the active class and removes them.
+      // Identifies elements with the active class and removes it from them
       if (item.classList.contains("card__answer--active")) {
-        item.classList.remove("card__answer--active");
-        // if the item is the one that was clicked, this adds the active state
+        closeAnswer(item);
+        // If the item is the one that was clicked, this adds the active state
       } else if (item === el) {
-        item.classList.add("card__answer--active");
-        item.style.height = item.scrollHeight;
+        openAnswer(item);
       }
     });
   });
